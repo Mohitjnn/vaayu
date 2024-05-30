7b7755954fe5e12637c8c80cd1d74f84d4837b3f
Mohitjnn <jnmohit29@gmail.com>
2024-05-31 03:59:33 +0530

    routing added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
+const Links = [
+  { name: "Home", url: "/" },
+  { name: "About Us", url: "/about" },
+  { name: "Contact Us", url: "/contact" },
+  { name: "NewsLetter", url: "/newsletter" },
+  { name: "Schools", url: "/schools" },
+];
+
 function Navbar() {
-  return <div className="w-full flex justify-center">Navbar</div>;
+  return (
+    <div className={"w-full flex justify-between px-7"}>
+      <div>Vaayu India</div>
+      <div>
+        {Links.map((link) => (
+          <Link href={link.url}>{link.name}</Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
