16b04d3a3eae877ddfd9af0af445fadedbefd9c2
Mohitjnn <jnmohit29@gmail.com>
2024-05-31 02:57:23 +0530

    initial commit

---
+++ src/app/schools/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function schools() {
+  return <div>schools</div>;
+}
+
+export default schools;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="w-full flex justify-center">Navbar</div>;
+}
+
+export default Navbar;
