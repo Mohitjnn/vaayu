b95c1a7ec7b9f0852bc074f30bae8b2266d9ef18
Mohitjnn <jnmohit29@gmail.com>
2024-05-31 04:49:48 +0530

    navbar made

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} w-full h-screen`}>
+      <body className={`${inter.className} w-full h-full`}>
         <div>
           <Navbar />
           {children}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,52 @@
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
-import React from "react";
+import { Ubuntu } from "next/font/google";
+
+const roboto = Ubuntu({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const Links = [
   { name: "Home", url: "/" },
   { name: "About Us", url: "/about" },
-  { name: "Contact Us", url: "/contact" },
   { name: "NewsLetter", url: "/newsletter" },
   { name: "Schools", url: "/schools" },
+  { name: "Contact Us", url: "/contact" },
 ];
 
 function Navbar() {
+  const [activeLink, setActiveLink] = useState(""); // Track clicked link
+
+  const handleClick = (url) => {
+    setActiveLink(url);
+  };
+
   return (
-    <div className={"w-full flex justify-between px-7"}>
-      <div>Vaayu India</div>
-      <div>
+    <div
+      className={` ${roboto.className} w-full flex justify-between items-center my-10 px-14`}
+    >
+      <Link
+        href="/"
+        className="text-3xl font-bold"
+        onClick={() => setActiveLink("/")}
+      >
+        Vaayu India
+      </Link>
+      <div className="flex space-x-10">
         {Links.map((link) => (
-          <Link href={link.url}>{link.name}</Link>
+          <Link
+            href={link.url}
+            key={link.url}
+            className={`text-xl hover:underline underline-offset-4 ${
+              activeLink === link.url ? "text-blue-500 underline" : ""
+            }`}
+            onClick={() => handleClick(link.url)}
+          >
+            {link.name}
+          </Link>
         ))}
       </div>
     </div>
