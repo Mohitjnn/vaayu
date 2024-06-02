eb651e2abcced21fb9c78bb949e366c4dc92c39c
Mohitjnn <jnmohit29@gmail.com>
2024-06-02 18:26:44 +0530

    footer made

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,71 @@
 import React from "react";
 
+import Image from "next/image";
 function Footer() {
-  return <div className="w-full bg-blue-500 h-32 mt-14">Footer</div>;
+  return (
+    <div className="w-full bg-blue-500 mt-8 h-fit px-14 py-4 bottom-0 flex justify-between items-center">
+      <div className="flex flex-col items-center justify-evenly w-fit h-fit space-y-3 px-16">
+        <Image src="/logoWhite.png" height={10} width={60} />
+        <h1 className="subHeading">VAAYUN INDIA</h1>
+        <div className="text text-center">
+          <p>Designed and Developed by</p>
+          <p>Mohit Jain</p>
+          <p className="text-lg">@2024|Vaayun|All Rights Reserved</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-evenly w-fit h-fit space-y-3 px-16">
+        <h1 className="subHeading">Contact</h1>
+        <div className="text text-center">
+          <p>
+            <span className="font-semibold px-2">Email:</span>
+            Creedaindiasports@gmail.com
+          </p>
+          <div className="flex flex-row justify-start w-full items-center">
+            <span className="font-semibold px-2">Phone:</span>
+            <p>+91 8689890988</p>
+            <p className="ml-3">+91 8291724184</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-evenly w-fit h-fit space-y-3 px-16">
+        <h1 className="subHeading">Socials</h1>
+        <div className="flex items-center space-x-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M16.19 2H7.81C4.17 2 2 4.17 2 7.81v8.37C2 19.83 4.17 22 7.81 22h8.37c3.64 0 5.81-2.17 5.81-5.81V7.81C22 4.17 19.83 2 16.19 2ZM12 15.88c-2.14 0-3.88-1.74-3.88-3.88 0-2.14 1.74-3.88 3.88-3.88 2.14 0 3.88 1.74 3.88 3.88 0 2.14-1.74 3.88-3.88 3.88Zm5.92-9c-.05.12-.12.23-.21.33-.1.09-.21.16-.33.21a.995.995 0 0 1-1.09-.21c-.09-.1-.16-.21-.21-.33A.995.995 0 0 1 16 6.5c0-.13.03-.26.08-.38.05-.13.12-.23.21-.33.23-.23.58-.34.9-.27.07.01.13.03.19.06.06.02.12.05.18.09.05.03.1.08.15.12.09.1.16.2.21.33.05.12.08.25.08.38s-.03.26-.08.38Z"
+              fill="#000000"
+            ></path>
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              fill="#000000"
+              d="M22 16.19c0 3.64-2.17 5.81-5.81 5.81H15c-.55 0-1-.45-1-1v-5.77c0-.27.22-.5.49-.5l1.76-.03c.14-.01.26-.11.29-.25l.35-1.91a.303.303 0 00-.3-.35l-2.13.03c-.28 0-.5-.22-.51-.49l-.04-2.45c0-.16.13-.3.3-.3l2.4-.04c.17 0 .3-.13.3-.3l-.04-2.4c0-.17-.13-.3-.3-.3l-2.7.04a2.996 2.996 0 00-2.95 3.05l.05 2.75c.01.28-.21.5-.49.51l-1.2.02c-.17 0-.3.13-.3.3l.03 1.9c0 .17.13.3.3.3l1.2-.02c.28 0 .5.22.51.49l.09 5.7c.01.56-.44 1.02-1 1.02h-2.3C4.17 22 2 19.83 2 16.18V7.81C2 4.17 4.17 2 7.81 2h8.38C19.83 2 22 4.17 22 7.81v8.38z"
+            ></path>
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+          >
+            <path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
