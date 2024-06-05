1c12974c19e9ca602be16cf9e79856cddc57df7e
Mohitjnn <jnmohit29@gmail.com>
2024-06-05 07:37:56 +0530

    contact us page made responsive

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,47 +1,47 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import Form1 from "@/components/Forms/Form1";
 import Form2 from "@/components/Forms/Form2";
 
 function Contact() {
   const formContainerRef = useRef(null);
+  const [isForm1Visible, setIsForm1Visible] = useState(true);
 
-  const scrollToBottom = () => {
-    formContainerRef.current.scrollTo({
-      top: formContainerRef.current.scrollHeight,
-      behavior: "smooth",
-    });
-  };
-
-  const scrollToTop = () => {
-    formContainerRef.current.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+  const scrollToForm = (form) => {
+    if (formContainerRef.current) {
+      formContainerRef.current.scrollTo({
+        top: form === "form1" ? 0 : formContainerRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+      setIsForm1Visible(form === "form1");
+    }
   };
 
   return (
     <main className="w-full h-full">
-      <center className="w-full h-full px-16">
-        <section className="flex flex-row w-full h-full justify-between items-start space-x-6">
-          <div className="w-[45%] relative h-[90vh] bg-custom-gradient rounded-br-[20%] my-5  ">
+      <center className="w-full h-full px-4 md:px-0 xl:px-16">
+        <section className="flex flex-col xl:flex-row w-full h-full justify-between items-start xl:space-x-6 xl:space-y-0">
+          <div className="hidden md:block md:w-full xl:w-[45%] relative md:h-[45vh] xl:h-[90vh] bg-custom-gradient  xl:rounded-br-[20%] xl:my-5">
             <Image
               src="/contactUsImg.png"
               fill={true}
               objectFit="cover"
               quality={100}
               priority={true}
+              alt="Contact Us"
             />
           </div>
           <div
-            className="flex flex-col w-[50%] h-[93vh] space-y-4 items-start justify-start overflow-hidden"
+            className="flex flex-col w-full xl:w-[50%] h-full space-y-4 items-start px-4 my-4 justify-start xl:justify-start overflow-y-auto"
             ref={formContainerRef}
           >
             <div className="flex w-full justify-between">
-              <h1 className="Title text-blue-500">Contact Us</h1>
+              <h1 className="Title text-blue-500">
+                {isForm1Visible ? "Contact Us" : "Join Us"}
+              </h1>
               <button
-                onClick={scrollToBottom}
+                onClick={() => scrollToForm(isForm1Visible ? "form2" : "form1")}
                 className="flex items-center justify-center w-10 h-10 bg-blue-500 rounded-full mt-4"
               >
                 <svg
@@ -55,35 +55,16 @@ function Contact() {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M19.5 12l-7.5 7.5L4.5 12m15-7.5l-7.5 7.5L4.5 4.5"
+                    d={
+                      isForm1Visible
+                        ? "M19.5 12l-7.5 7.5L4.5 12m15-7.5l-7.5 7.5L4.5 4.5"
+                        : "M4.5 12l7.5-7.5 7.5 7.5m-15 7.5l7.5-7.5 7.5 7.5"
+                    }
                   />
                 </svg>
               </button>
             </div>
-            <Form1 />
-            <div className="flex w-full justify-between">
-              <h1 className="Title text-blue-500">Join Us</h1>
-              <button
-                onClick={scrollToTop}
-                className="flex items-center justify-center w-10 h-10 bg-blue-500 rounded-full mt-4"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6 text-white"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12l7.5-7.5 7.5 7.5m-15 7.5l7.5-7.5 7.5 7.5"
-                  />
-                </svg>
-              </button>
-            </div>
-            <Form2 />
+            {isForm1Visible ? <Form1 /> : <Form2 />}
           </div>
         </section>
       </center>
