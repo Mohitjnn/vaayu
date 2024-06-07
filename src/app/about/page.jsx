0e9059654ac08eff3742e627f91c166903e1e290
Mohitjnn <jnmohit29@gmail.com>
2024-06-07 17:58:15 +0530

    datacard updation

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -146,43 +146,14 @@ const About = () => {
         <section className="w-full h-fit px-4 lg:px-12 space-y-4 flex flex-col items-center ">
           <h1 className="Title xl:Heading">Why Choose Us</h1>
           <div
-            className="flex flex-row justify-start w-full overflow-x-auto overflow-y-hidden scroll-smooth xl:pt-8 xl:overflow-x-hidden h-full no-scrollbar"
+            className="flex flex-row justify-start w-full overflow-x-auto overflow-y-hidden scroll-smooth xl:py-8  h-full no-scrollbar"
             ref={formContainerRef}
           >
             {response.aboutUsList1.map((reasons, index) => (
               <Datacard Reasons={reasons} key={index} />
             ))}
           </div>
-          <div className="hidden xl:flex flex-row justify-center space-x-4 w-full h-fit">
-            <button onClick={scrollLeft}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="60"
-                height="60"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
-                <path
-                  fill="#3b82f6"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm3.5 10.75h-5.19l1.72 1.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22s-.38-.07-.53-.22l-3-3a.754.754 0 010-1.06l3-3c.29-.29.77-.29 1.06 0 .29.29.29.77 0 1.06l-1.72 1.72h5.19c.41 0 .75.34.75.75s-.34.75-.75.75z"
-                ></path>
-              </svg>
-            </button>
-            <button onClick={scrollRight}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="60"
-                height="60"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
-                <path
-                  fill="#3b82f6"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.03 10.53l-3 3c-.15.15-.34.22-.53.22s-.38-.07-.53-.22a.754.754 0 010-1.06l1.72-1.72H8.5c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h5.19l-1.72-1.72a.754.754 0 010-1.06c.29-.29.77-.29 1.06 0l3 3c.29.29.29.77 0 1.06z"
-                ></path>
-              </svg>
-            </button>
-          </div>
+          <div className="hidden xl:flex flex-row justify-center space-x-4 w-full h-fit"></div>
         </section>
       </center>
     </main>
--- src/components/DataCard/Datacard.jsx
+++ src/components/DataCard/Datacard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function Datacard({ Reasons }) {
   return (
-    <div className="flex flex-col h-fit w-full mx-10 ml-0 md:ml-16 lg:mx-16 my-4 transition-transform duration-300 ease-in-out transform hover:scale-110">
+    <div className="flex flex-col h-fit w-full p-4 mx-10 ml-0 md:ml-16 lg:mx-16 my-4 transition-transform duration-300 ease-in-out transform hover:scale-110 xl:shadow-xl xl:hover:shadow-2xl xl:rounded-3xl">
       <div className="w-[50vh] h-[45vh] md:h-[50vh] relative ">
         <Image
           src={`${Reasons.imageSrc}`}
@@ -13,7 +13,7 @@ function Datacard({ Reasons }) {
         />
       </div>
       <h2 className="text-xl font-bold mt-4">{Reasons.Title}</h2>
-      <p className="text-m mt-2 pb-6 px-4 h-full text-center">
+      <p className="text-m mt-2 pb-6 px-3 h-full text-center">
         {Reasons.Description}
       </p>
     </div>
