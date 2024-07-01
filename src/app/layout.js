4f67d96dbaac28a487a195af0dfc67f401ed2929
Mohitjnn <jnmohit29@gmail.com>
2024-07-01 15:35:55 +0530

    maintenance completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,10 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${poppins.className} w-full h-full`}>
-        <center className="Heading">Under Maintainance</center>
-        {/* <Navbar />
+        {/* <center className="Heading">Under Maintainance</center> */}
+        <Navbar />
         {children}
-        <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
