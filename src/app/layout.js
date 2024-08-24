9b21e8fa5a8b7c5017a7e85ad55851062a6ea00e
Mohitjnn <jnmohit29@gmail.com>
2024-08-24 17:33:54 +0530

    opengraph image added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,39 +14,45 @@ export const metadata = {
   },
   description:
     "Empowering The next generation of athletes: Revolutionizing sports education",
-  openGraph: {
-    url: "https://vaayu-one.vercel.app/",
-    type: "website",
-    title: "Vaayun",
-    description:
-      "Empowering The next generation of athletes: Revolutionizing sports education",
-    images: [
-      {
-        url: "https://opengraph.b-cdn.net/production/images/252f37cf-c793-439f-8f12-b4bb468ad7ae.png?token=8a1w5iNAGbYSlLS5oEY1QyRerB7b_gXEvFYgxzKaSCI&height=630&width=1200&expires=33260500366",
-        width: 1200,
-        height: 630,
-        alt: "Vaayun - Empowering The next generation of athletes",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    domain: "vaayu-one.vercel.app",
-    url: "https://vaayu-one.vercel.app/",
-    title: "Vaayun",
-    description:
-      "Empowering The next generation of athletes: Revolutionizing sports education",
-    images: [
-      {
-        url: "https://opengraph.b-cdn.net/production/images/252f37cf-c793-439f-8f12-b4bb468ad7ae.png?token=8a1w5iNAGbYSlLS5oEY1QyRerB7b_gXEvFYgxzKaSCI&height=630&width=1200&expires=33260500366",
-      },
-    ],
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Vaayun</title>
+        <meta
+          name="description"
+          content="Empowering The next generation of athletes: Revolutionizing sports education"
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://vaayu-one.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Vaayun" />
+        <meta
+          property="og:description"
+          content="Empowering The next generation of athletes: Revolutionizing sports education"
+        />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/images/252f37cf-c793-439f-8f12-b4bb468ad7ae.png?token=8a1w5iNAGbYSlLS5oEY1QyRerB7b_gXEvFYgxzKaSCI&height=630&width=1200&expires=33260500366"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="vaayu-one.vercel.app" />
+        <meta property="twitter:url" content="https://vaayu-one.vercel.app/" />
+        <meta name="twitter:title" content="Vaayun" />
+        <meta
+          name="twitter:description"
+          content="Empowering The next generation of athletes: Revolutionizing sports education"
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/252f37cf-c793-439f-8f12-b4bb468ad7ae.png?token=8a1w5iNAGbYSlLS5oEY1QyRerB7b_gXEvFYgxzKaSCI&height=630&width=1200&expires=33260500366"
+        />
+      </head>
       <body className={`${poppins.className} w-full h-full scroll-smooth`}>
         <PolyfillProvider>
           <SplashScreenManager>
