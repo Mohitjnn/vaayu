cb1cd85e76d12b91f4b64fbe17bd3b632c99e6ab
Mohitjnn <jnmohit29@gmail.com>
2026-08-20 03:23:35 +0530

    datafgetch update

--- src/customHook/datafetch.js
+++ src/customHook/datafetch.js
@@ -1,6 +1,6 @@
 async function getData() {
-  // const response = await fetch(`${process.env.URL}/data.json`);
-  const response = await fetch("http://localhost:3000/data.json", { cache: "no-store" });
+  const response = await fetch(`${process.env.URL}/data.json`,{ cache: "no-store" });
+  // const response = await fetch("http://localhost:3000/data.json", { cache: "no-store" });
 
   return response.json();
 }
